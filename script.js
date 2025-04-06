f17593d20be5b7ad1bb15273ed184887c6b46459
Juan <juanysepu7@gmail.com>
2025-04-06 14:45:55 -0300

    HTML y JS terminados

---
+++ script.js
@@ -0,0 +1,38 @@
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const searchInput = document.getElementById("search");
+const pokedexContainer = document.getElementById("pokedex");
+
+function showError(mensaje) {
+    pokedexContainer.innerHTML = `<p>${mensaje}`;
+}
+
+async function searchPokemon() {
+    const searchedPokemon = searchInput.value.toLowerCase();
+
+    try {
+        const response = await fetch(URL + searchedPokemon);
+        
+        if (!response.ok) {
+            showError(`No se encontró ningún Pokémon llamado: ${searchedPokemon}`);
+            return;
+        }
+
+        const data = await response.json();
+
+        pokedexContainer.innerHTML = 
+        `
+            <h2>${data.name.toUpperCase()}</h2>
+            <img src="${data.sprites.front_default}">
+            <p>Número: ${data.id}</p>
+            <p>Altura: ${data.height / 10}m</p>
+            <p>Peso: ${data.weight /10}kg</p>
+        `
+    } catch (error) {
+        console.log(error);
+        showError("Ha ocurrido un error al buscar el Pokémon")
+    }
+}
+
+document.getElementById("btn-search").addEventListener("click", searchPokemon);
+
